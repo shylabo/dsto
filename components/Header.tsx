24e1f85889aefc8bf3497272e3a7c772dc39e4fd
shylabo <shuya.sohot@gmail.com>
2024-01-09 22:52:57 -0800

    fix: toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,15 +13,19 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ className }) => {
   const [isOpen, setOpen] = useState<boolean>(false)
-
-  const handleMenuToggle = () => {
+  const handleMenuOpen = () => {
     setOpen(!isOpen)
+    document.body.style.overflow = isOpen ? 'auto' : 'hidden'
+  }
+
+  const handleMenuClose = () => {
+    setOpen(false)
     document.body.style.overflow = isOpen ? 'auto' : 'hidden'
   }
 
   return (
     <header className={cn('flex items-center justify-between w-full p-4', className)}>
-      <Link href="/" className="inline-block z-50" onClick={handleMenuToggle}>
+      <Link href="/" className="inline-block z-50" onClick={handleMenuClose}>
         <Image src="/images/logo.png" alt="company logo" width={40} height={40} />
       </Link>
 
@@ -29,7 +33,7 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
         <ul className={isOpen ? 'flex h-screen justify-center items-center flex-col gap-y-12 text-xl' : 'hidden'}>
           {navItems.map((item) => (
             <li key={item.value}>
-              <Link onClick={handleMenuToggle} href={item.destination}>
+              <Link onClick={handleMenuOpen} href={item.destination}>
                 {item.label}
               </Link>
             </li>
@@ -38,7 +42,7 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
         </ul>
       </nav>
 
-      <button className="z-50 space-y-2 md:hidden" onClick={handleMenuToggle}>
+      <button className="z-50 space-y-2 md:hidden" onClick={handleMenuOpen}>
         <span
           className={
             isOpen
