1b5d9fb75a32c447322a193840a5720b0d1e51c7
shylabo <shuya.sohot@gmail.com>
2024-02-01 14:18:47 -0800

    fix bug

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -2,7 +2,7 @@ import { FaInstagram } from 'react-icons/fa6'
 
 const page = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-7 min-h-[calc(100vh-75px)] md:min-h-screen pb-[75px] md:p-0">
+    <div className="flex flex-col justify-center items-center gap-y-7 min-h-[calc(100vh-75px)] md:-ml-[330px] md:min-h-screen pb-[75px] md:pb-0">
       <a href="mailto:keishi.tomiya@d-s-to.com">keishi.tomiya@d-s-to.com</a>
       <a href="https://www.instagram.com/d_s_to" target="_blank" rel="noopener noreferrer">
         <FaInstagram size={24} />
