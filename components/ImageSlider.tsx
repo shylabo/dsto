3a9cb2877efe42a4c10959dcc3dada6dd0fed2ca
shylabo <shuya.sohot@gmail.com>
2024-04-04 22:05:16 -0700

    fix: header

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -20,7 +20,7 @@ const ImageSlider: React.FC = () => {
   }, [])
 
   return (
-    <div className="relative w-full h-[calc(100vh-75px)] overflow-y-hidden -mb-[75px] lg:h-full">
+    <div className="relative w-full h-[calc(100vh-75px)] lg:h-full">
       {images.map((image, index) => (
         <Image
           key={index}
