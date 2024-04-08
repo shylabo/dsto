8c1f269bfab6e7d6d0443367c03e8a1ca1fb2129
shylabo <shuya.sohot@gmail.com>
2024-04-08 11:35:49 -0700

    fix: overflow content

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -40,7 +40,7 @@ const LocaleLayout: React.FC<LocaleLayoutProps> = async ({ children, params: { l
         <NextIntlClientProvider locale={locale} messages={messages}>
           <SideMenu className="sticky top-0 hidden lg:block min-w-[330px]" />
           <Header className="lg:hidden h-[70px] sm:h-[130px]" />
-          <main className="min-h-[calc(100dvh-70px)] sm:min-h-[calc(100dvh-130px)] lg:min-h-screen w-full">
+          <main className="min-h-[calc(100dvh-70px)] sm:min-h-[calc(100dvh-130px)] lg:min-h-screen w-full md:max-w-[calc(100vw-330px)] overflow-x-hidden">
             {children}
           </main>
         </NextIntlClientProvider>
