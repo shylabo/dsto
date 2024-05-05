92108efa4148b778b5681b5098b3fb940b66d2db
shylabo <shuya.sohot@gmail.com>
2024-05-05 18:30:56 -0400

    fix: height in contact

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -2,7 +2,7 @@ import { FaInstagram } from 'react-icons/fa6'
 
 const page = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-7 h-[calc(100dvh-70px)] lg:-ml-[330px] lg:h-screen pb-[75px] lg:pb-0">
+    <div className="flex flex-col justify-center items-center gap-y-7 h-[calc(100dvh-92px)] sm:h-[calc(100dvh-130px)] lg:h-screen lg:-ml-[330px] pb-[92px] sm:pb-[130px] lg:pb-0">
       <a href="mailto:keishi.tomiya@d-s-to.com">keishi.tomiya@d-s-to.com</a>
       <a href="https://www.instagram.com/d_s_to" target="_blank" rel="noopener noreferrer">
         <FaInstagram size={24} />
