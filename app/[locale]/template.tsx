fe6955152849f0457693d22d42f31867f9bcd943
shylabo <shuya.sohot@gmail.com>
2024-05-05 18:11:06 -0400

    update: add page transition animation

---
+++ app/[locale]/template.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import Transition from '@/components/Transition'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return <Transition>{children}</Transition>
+}
